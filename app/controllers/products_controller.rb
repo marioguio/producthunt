ff42f95db362397d91a3cb695826b14134ab96b3
marioguio <mario_guio@outlook.com>
2017-08-25 16:45:13 -0500

    Crea la ruta /products/:id/edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,22 +2,27 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
   def show
     @product = Product.find(params[:id])
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
   def new
     @product = Product.new
   end
+
   def create
-    @product= Product.new(product_params)
+    @product = Product.new(product_params)
     if @product.save
       redirect_to products_path, notice: "El producto fue publicado con éxito"
     else
       render :new
     end
-    def edit
-      @product = Product.find(params[:id])
-    end
+
   end
   private
   def product_params
